8527aea158efb2bc1d26874cdb627c2d7dbec07b
alexaz-za <alexsudlaa@gmail.com>
2026-01-27 00:18:13 +0700

    2513110664 LabSheet01

---
+++ LabSheet01/src/Ex102.java
@@ -0,0 +1,8 @@
+public class Ex102 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Anawat Sudla");
+        System.out.println("\"INT-108\" \'Object-Oriented Programming\'");
+        System.out.println("\tThis is my first time learning Java");
+        System.out.println("\tI hope to get grade \"A\"");
+    }
+}
---
+++ LabSheet01/src/Ex103.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Ex103 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Hello, I am Anawat Sudla",
+                "Welcome message",
+                JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null,
+                "Hello, I am Anawat Sudla",
+                "Welcome message",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}
